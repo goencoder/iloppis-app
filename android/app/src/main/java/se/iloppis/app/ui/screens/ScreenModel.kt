024ab861b8ba0e7351f54ceec591b015764632e7
Xtarii <carlalvinelias@gmail.com>
2026-01-20 19:55:26 +0100

    Basic page loading system

--- android/app/src/main/java/se/iloppis/app/ui/screens/ScreenModel.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/ScreenModel.kt
@@ -1,5 +1,6 @@
 package se.iloppis.app.ui.screens
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
@@ -10,6 +11,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import se.iloppis.app.ui.states.ScreenAction
+import se.iloppis.app.ui.states.ScreenPage
 import se.iloppis.app.ui.states.ScreenState
 
 /**
@@ -30,12 +32,14 @@ class ScreenModel : ViewModel() {
     fun onAction(action: ScreenAction) {
         when(action) {
             is ScreenAction.Loading -> setLoad(action.status)
+            is ScreenAction.LoadPage -> loadPage(action.page)
         }
     }
 
 
 
     private fun setLoad(state: Boolean) { this.state = this.state.copy(isLoading = state) }
+    private fun loadPage(page: ScreenPage) { state = state.copy(page = page) }
 }
 
 

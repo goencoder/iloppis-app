287679da20219b872664cc5497bd715d799e108d
Xtarii <carlalvinelias@gmail.com>
2026-01-20 19:17:16 +0100

    Basic base for a navigator

--- android/app/src/main/java/se/iloppis/app/ui/components/navigation/Navigator.kt
+++ android/app/src/main/java/se/iloppis/app/ui/components/navigation/Navigator.kt
@@ -1,21 +1,20 @@
 package se.iloppis.app.ui.components.navigation
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import se.iloppis.app.ui.screens.screenContext
-
-/**
- * Navigation color alpha
- */
-const val NAVIGATOR_ALPHA = 0.3f
-
-
 
 /**
  * Application navigator
@@ -29,20 +28,22 @@ const val NAVIGATOR_ALPHA = 0.3f
  * @see se.iloppis.app.ui.screens.ScreenModel
  */
 @Composable
-fun Navigator(content: @Composable () -> Unit) {
+fun Navigator(height: Dp = 120.dp, paddingValues: Dp = 25.dp, alpha: Float = 0.4f) {
     val screen = screenContext()
 
-    Scaffold(
-        modifier = Modifier,
-        bottomBar = {
-            NavigationBar(
-                containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = NAVIGATOR_ALPHA)
-            ) {
-            }
-        }
-    ) { paddingValues ->
-        Box(modifier = Modifier.padding(paddingValues).fillMaxWidth()) {
-            content()
+    NavigationBar(
+        modifier = Modifier.height(height),
+        containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha)
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = paddingValues,
+                top = paddingValues,
+                bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding(),
+                end = paddingValues
+            )
+        ) {
         }
     }
 }

a540c3a085df366ebf3e282f28212c1077433ef1
Xtarii <carlalvinelias@gmail.com>
2026-01-22 20:37:00 +0100

    Selection strings ( Scanner )

--- android/app/src/main/java/se/iloppis/app/ui/screens/scanner/ScannerSelectionScreen.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/scanner/ScannerSelectionScreen.kt
@@ -1,4 +1,26 @@
 package se.iloppis.app.ui.screens.scanner
 
-class ScannerSelectionScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import se.iloppis.app.ui.screens.events.EventListHeader
+
+/**
+ * Scanner selection screen
+ *
+ * This uses the [se.iloppis.app.utils.LocalStorage] bucket
+ * to get stored events for scanner selection.
+ */
+@Composable
+fun ScannerSelectionScreen() {
+    Column(modifier = Modifier.fillMaxSize()
+        .padding(horizontal = 16.dp)
+        .statusBarsPadding()
+    ) {
+        EventListHeader()
+    }
 }

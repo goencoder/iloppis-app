3775943e08b08151c31ed120b611787f48fe90c1
Xtarii <carlalvinelias@gmail.com>
2026-01-22 19:23:24 +0100

    swipe card

--- android/app/src/main/java/se/iloppis/app/ui/components/events/EventCards.kt
+++ android/app/src/main/java/se/iloppis/app/ui/components/events/EventCards.kt
@@ -1,16 +1,25 @@
 package se.iloppis.app.ui.components.events
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CheckBox
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import se.iloppis.app.R
 import se.iloppis.app.domain.model.Event
 import se.iloppis.app.ui.components.EventCard
 
@@ -35,14 +44,41 @@ fun SwipeToDismissEventCard(
         enableDismissFromStartToEnd = enableStartToEnd,
         enableDismissFromEndToStart = enableEndToStart,
         backgroundContent = {
-            val color by animateColorAsState(
-                when(state.targetValue) {
-                    SwipeToDismissBoxValue.Settled -> MaterialTheme.colorScheme.background
-                    SwipeToDismissBoxValue.EndToStart -> MaterialTheme.colorScheme.primary
-                    SwipeToDismissBoxValue.StartToEnd -> MaterialTheme.colorScheme.surface
+            when(state.dismissDirection) {
+                SwipeToDismissBoxValue.StartToEnd -> {
+                    Icon(
+                        imageVector = Icons.Outlined.CheckBox,
+                        contentDescription = stringResource(R.string.swipe_box_archive),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(lerp(
+                                MaterialTheme.colorScheme.background,
+                                MaterialTheme.colorScheme.secondary,
+                                state.progress
+                            ), shape = RoundedCornerShape(16.dp))
+                            .wrapContentSize(Alignment.CenterStart)
+                            .padding(12.dp),
+                        tint = MaterialTheme.colorScheme.background
+                    )
                 }
-            )
-            Box(modifier = Modifier.fillMaxSize().background(color))
+                SwipeToDismissBoxValue.EndToStart -> {
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = stringResource(R.string.swipe_box_remove),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(lerp(
+                                MaterialTheme.colorScheme.background,
+                                MaterialTheme.colorScheme.error,
+                                state.progress
+                            ), shape = RoundedCornerShape(16.dp))
+                            .wrapContentSize(Alignment.CenterEnd)
+                            .padding(12.dp),
+                        tint = MaterialTheme.colorScheme.background
+                    )
+                }
+                SwipeToDismissBoxValue.Settled -> {}
+            }
         },
         onDismiss = {
             if(it == SwipeToDismissBoxValue.EndToStart) onEndToStart()

f4123f011d55ce77ffac790fbd2f8ceffeda874b
Xtarii <carlalvinelias@gmail.com>
2026-01-24 23:30:32 +0100

    Network Unit tests

--- android/app/src/test/java/se/iloppis/app/data/network/TodayTimeTest.kt
+++ android/app/src/test/java/se/iloppis/app/data/network/TodayTimeTest.kt
@@ -1,1 +1,22 @@
-package se.iloppis.app.data.network 
+package se.iloppis.app.data.network
+
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import se.iloppis.app.network.getTodayTime
+import java.time.LocalDate
+
+class TodayTimeTest {
+    @Test
+    fun `Compares old time format to new time format`() = runTest {
+        val check = "${LocalDate.now()}T00:00:00Z"
+        val res = getTodayTime() /* System clock */
+        Assert.assertEquals(
+            "\n======== Today ( System clock ) ========\n" +
+                    "\t$check != $res\n" +
+                    "======== Today ( System clock ) ========\n\n",
+            check,
+            res
+        )
+    }
+}

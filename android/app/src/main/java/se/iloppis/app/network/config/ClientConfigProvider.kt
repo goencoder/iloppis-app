628f1a88ddc0d24c9e3ab7f3537356493f85b803
Xtarii <carlalvinelias@gmail.com>
2026-01-29 19:33:35 +0100

    Client config provider

--- android/app/src/main/java/se/iloppis/app/network/config/ClientConfigProvider.kt
+++ android/app/src/main/java/se/iloppis/app/network/config/ClientConfigProvider.kt
@@ -1,1 +1,40 @@
 package se.iloppis.app.network.config
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+
+/**
+ * Local client config composition
+ */
+private val localClientConfig = compositionLocalOf<ClientConfig> {
+    error("No client config provider is present in this context")
+}
+
+
+
+/**
+ * iLoppis client configuration provider
+ *
+ * This will store the [config] provided and
+ * make it available for all [Composable]
+ * components in this local context.
+ */
+@Composable
+fun ClientConfigProvider(config: ClientConfig, content: @Composable () -> Unit) {
+    CompositionLocalProvider(localClientConfig provides config) {
+        content()
+    }
+}
+
+
+
+/**
+ * iLoppis client configuration
+ *
+ * This uses the [ClientConfigProvider] to
+ * provide the local context with a
+ * client configuration for the [se.iloppis.app.network.iLoppisClient]
+ */
+@Composable
+fun clientConfig() : ClientConfig = localClientConfig.current
--- android/app/src/main/java/se/iloppis/app/network/iLoppisClient.kt
+++ android/app/src/main/java/se/iloppis/app/network/iLoppisClient.kt
@@ -4,6 +4,10 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import se.iloppis.app.network.config.ClientConfig
+import se.iloppis.app.network.config.ClientConnectionConfiguration
+import se.iloppis.app.network.config.ClientReadingConfiguration
+import se.iloppis.app.network.config.ClientWritingConfiguration
 import java.util.concurrent.TimeUnit
 
 /**

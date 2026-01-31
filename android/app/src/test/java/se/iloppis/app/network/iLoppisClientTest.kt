d6c5b3271f6240b020b3d367feaffcfbc898d6f8
Xtarii <carlalvinelias@gmail.com>
2026-01-31 23:14:45 +0100

    updated test

--- android/app/src/test/java/se/iloppis/app/network/iLoppisClientTest.kt
+++ android/app/src/test/java/se/iloppis/app/network/iLoppisClientTest.kt
@@ -5,6 +5,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import se.iloppis.app.network.config.ClientConfig
 import se.iloppis.app.network.events.EventAPI
+import se.iloppis.app.network.events.EventLifecycle
 
 class iLoppisClientTest {
     val config: ClientConfig = ClientConfig("https://iloppis-staging.fly.dev/")
@@ -41,7 +42,7 @@ class iLoppisClientTest {
                     "\t resEvents.total != 1\n" +
                     "\n========= Events =========\n\n",
             1,
-            resMarket.total
+            resEvents.total
         )
     }
 
@@ -54,29 +55,23 @@ class iLoppisClientTest {
         // ==================================
 
         val client = iLoppisClient(config).create<EventAPI>()
-
-        val market = "ed71222f-36ad-40a6-a102-2fb821bed1c0"
-        val events = "66af78e4-be70-4145-be55-0c3cdfe6637c,66af78e4-be70-4145-be55-0c3cdfe6637d"
-
-        val resMarket = client.getEventsFromMarkets(market)
+        val events = "66af78e4-be70-4145-be55-0c3cdfe6637c"
         val resEvents = client.get(events)
 
-        println("\nMarkets: $resMarket")
         println("Events: $resEvents\n")
-
-        assertEquals(
-            "\n========= Market =========\n" +
-                    "\t resMarket.total != 1\n" +
-                    "\n========= Market =========\n\n",
-            1,
-            resMarket.total
+        println(
+            "Checks if states are parsed correctly\n" +
+            "=====================================\n" +
+            "Expected ${EventLifecycle.OPEN}\n" +
+            "Result ${resEvents.events[0].lifecycleState}"
         )
+
         assertEquals(
             "\n========= Events =========\n" +
                     "\t resEvents.total != 1\n" +
                     "\n========= Events =========\n\n",
-            1,
-            resMarket.total
+            EventLifecycle.OPEN,
+            resEvents.events[0].lifecycleState
         )
     }
 }

e0fded74b0cc85b06bec0162ad91b763e748d687
Xtarii <carlalvinelias@gmail.com>
2026-02-06 21:36:46 +0100

    Maplibre extension utils

---
+++ android/app/src/main/java/se/iloppis/app/utils/map/GeoJson.kt
@@ -0,0 +1,27 @@
+package se.iloppis.app.utils.map
+
+/**
+ * Maplibre GeoJson source data object
+ */
+data class GeoJsonSource(
+//    "sources": {
+//    "geojson-marker": {
+//        "type": "geojson",
+//        "data": {
+//            "type": "Feature",
+//            "geometry": {
+//            "type": "Point",
+//            "coordinates": [12.550343, 55.665957]
+//        },
+//            "properties": {
+//            "title": "Somewhere",
+//            "marker-symbol": "monument"
+//        }
+//        }
+//    },
+//    "geojson-lines": {
+//        "type": "geojson",
+//        "data": "./lines.geojson"
+//    }
+//}
+)

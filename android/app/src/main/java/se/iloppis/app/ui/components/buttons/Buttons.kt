79fa57c1004dede00af4a38b2ef2888e50e8282b
Xtarii <carlalvinelias@gmail.com>
2026-02-07 23:09:30 +0100

    Icon and text button

--- android/app/src/main/java/se/iloppis/app/ui/components/buttons/Buttons.kt
+++ android/app/src/main/java/se/iloppis/app/ui/components/buttons/Buttons.kt
@@ -1,1 +1,40 @@
 package se.iloppis.app.ui.components.buttons
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+/**
+ * Button component with an icon and text object
+ */
+@Composable
+fun IconButton(
+    modifier: Modifier = Modifier,
+    text: Int,
+    icon: ImageVector,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    onclick: () -> Unit
+) {
+    Button(
+        onClick = onclick,
+        modifier = modifier,
+        colors = colors
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = stringResource(text)
+        )
+        Text(
+            text = stringResource(text),
+            modifier = Modifier.padding(start = 5.dp)
+        )
+    }
+}

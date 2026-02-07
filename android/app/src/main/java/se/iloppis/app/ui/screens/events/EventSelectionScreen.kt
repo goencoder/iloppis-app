0f0482f413889fb526b9182acde87f0abb04a998
Xtarii <carlalvinelias@gmail.com>
2026-02-07 23:30:34 +0100

    Added a common screen for event selection

--- android/app/src/main/java/se/iloppis/app/ui/screens/events/EventSelectionScreen.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/events/EventSelectionScreen.kt
@@ -1,1 +1,20 @@
 package se.iloppis.app.ui.screens.events
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import se.iloppis.app.domain.model.Event
+
+@Composable
+fun EventSelectionScreen(onAction: (event: Event) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .statusBarsPadding()
+    ) { }
+}

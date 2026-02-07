ce3edc2b7e11c3369483861b0cc6d4ff8e8b263a
Xtarii <carlalvinelias@gmail.com>
2026-02-07 23:42:13 +0100

    Home screen handles selection of events

--- android/app/src/main/java/se/iloppis/app/ui/screens/home/HomeScreen.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/home/HomeScreen.kt
@@ -18,11 +18,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import se.iloppis.app.R
+import se.iloppis.app.navigation.ScreenPage
 import se.iloppis.app.ui.components.buttons.IconButton
+import se.iloppis.app.ui.screens.events.CodeEntryMode
+import se.iloppis.app.ui.screens.events.EventListAction
 import se.iloppis.app.ui.screens.events.ILoppisHeader
+import se.iloppis.app.ui.screens.events.eventContext
+import se.iloppis.app.ui.screens.screenContext
+import se.iloppis.app.ui.states.ScreenAction
 
 @Composable
 fun HomeScreen() {
+    val screen = screenContext()
+    val event = eventContext()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,7 +49,21 @@ fun HomeScreen() {
                 text = R.string.home_open_cashier,
                 icon = Icons.Outlined.Payments
             ) {
-                /* Open event selection screen with parameter "CASHIER" */
+                screen.onAction(
+                    ScreenAction.NavigateToPage(
+                        ScreenPage.Selection {
+
+                            /* Event context */
+                            event.onAction(
+                                EventListAction.StartCodeEntry(
+                                    CodeEntryMode.CASHIER,
+                                    it
+                                )
+                            )
+
+                        }
+                    )
+                )
             }
 
             IconButton(
@@ -50,7 +73,21 @@ fun HomeScreen() {
                 ),
                 icon = Icons.Outlined.QrCode
             ) {
-                /* Open event selection screen with parameter "SCANNER" */
+                screen.onAction(
+                    ScreenAction.NavigateToPage(
+                        ScreenPage.Selection {
+
+                            /* Event context */
+                            event.onAction(
+                                EventListAction.StartCodeEntry(
+                                    CodeEntryMode.SCANNER,
+                                    it
+                                )
+                            )
+
+                        }
+                    )
+                )
             }
         }
     }

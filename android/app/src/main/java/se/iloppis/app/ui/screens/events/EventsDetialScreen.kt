13b398358c84f967ba8ce9c9abee8cfae6596c24
Xtarii <carlalvinelias@gmail.com>
2026-02-07 18:48:24 +0100

    added follow switch to event details

--- android/app/src/main/java/se/iloppis/app/ui/screens/events/EventsDetialScreen.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/events/EventsDetialScreen.kt
@@ -11,7 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,12 +27,14 @@ import androidx.compose.ui.unit.sp
 import se.iloppis.app.R
 import se.iloppis.app.domain.model.Event
 import se.iloppis.app.navigation.ScreenPage
+import se.iloppis.app.ui.components.StarIcon
 import se.iloppis.app.ui.components.StateBadge
 import se.iloppis.app.ui.components.map.Map
 import se.iloppis.app.ui.components.navigation.SwipeToNavigate
 import se.iloppis.app.ui.components.text.MarkdownText
 import se.iloppis.app.ui.screens.screenContext
 import se.iloppis.app.ui.states.ScreenAction
+import se.iloppis.app.utils.events.localEventsStorage
 
 @Composable
 fun EventsDetailsScreen(event: Event)  {
@@ -68,10 +73,11 @@ fun EventsDetailsScreen(event: Event)  {
                     ) {
                         Spacer(modifier = Modifier.height(7.dp))
                         EventDetailsContent(event)
+                        Spacer(modifier = Modifier.height(14.dp))
                     }
                 }
 
-                item { Spacer(modifier = Modifier.height(screen.border.calculateBottomPadding())) }
+                item {Spacer(modifier = Modifier.height(screen.border.calculateBottomPadding())) }
             }
         }
     }
@@ -84,6 +90,7 @@ fun EventsDetailsScreen(event: Event)  {
  */
 @Composable
 private fun EventDetailsContent(event: Event) {
+    val storage = localEventsStorage()
     Row(
         modifier = Modifier.fillMaxWidth().statusBarsPadding(),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -91,11 +98,18 @@ private fun EventDetailsContent(event: Event) {
     ) {
         Text(
             text = event.name,
+            modifier = Modifier.widthIn(max = 275.dp),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             color = MaterialTheme.colorScheme.tertiary
         )
-        StateBadge(event.state)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if(storage.contains(event.id)) StarIcon()
+            StateBadge(event.state)
+        }
     }
 
     Text(
@@ -126,5 +140,27 @@ private fun EventDetailsContent(event: Event) {
         )
         Spacer(modifier = Modifier.height(12.dp))
         MarkdownText(event.description)
+    }
+
+    // Follow event button
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors().copy(
+            containerColor = if(!storage.contains(event.id))
+                MaterialTheme.colorScheme.onSecondary
+            else MaterialTheme.colorScheme.primary
+        ),
+        onClick = {
+            if(!storage.contains(event.id)) storage.add(event.id)
+            else storage.remove(event.id)
+        }
+    ) {
+        Text(
+            text = if(!storage.contains(event.id))
+                stringResource(R.string.store_event_locally)
+            else
+                stringResource(R.string.remove_event_locally),
+            fontWeight = FontWeight.Medium
+        )
     }
 }

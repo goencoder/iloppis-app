e0ac3bf1cc63a962dd5226440efdeb41e284aa31
Xtarii <carlalvinelias@gmail.com>
2026-02-07 23:09:49 +0100

    Added cashier and scanner buttons to home

--- android/app/src/main/java/se/iloppis/app/ui/screens/home/HomeScreen.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/home/HomeScreen.kt
@@ -1,12 +1,24 @@
 package se.iloppis.app.ui.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Payments
+import androidx.compose.material.icons.outlined.QrCode
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import se.iloppis.app.R
+import se.iloppis.app.ui.components.buttons.IconButton
 import se.iloppis.app.ui.screens.events.ILoppisHeader
 
 @Composable
@@ -19,6 +31,27 @@ fun HomeScreen() {
     ) {
         ILoppisHeader()
 
-        /* Cashier and Scanner buttons */
+        Spacer(modifier = Modifier.height(14.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            IconButton(
+                text = R.string.home_open_cashier,
+                icon = Icons.Outlined.Payments
+            ) {
+                /* Open event selection screen with parameter "CASHIER" */
+            }
+
+            IconButton(
+                text = R.string.home_open_scanner,
+                colors = ButtonDefaults.buttonColors().copy(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                ),
+                icon = Icons.Outlined.QrCode
+            ) {
+                /* Open event selection screen with parameter "SCANNER" */
+            }
+        }
     }
 }

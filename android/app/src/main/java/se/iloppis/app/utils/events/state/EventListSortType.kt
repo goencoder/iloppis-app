219167e1286ed7357016d0459aa482496320f61b
Xtarii <carlalvinelias@gmail.com>
2026-02-08 17:41:13 +0100

    Added event list state and sort

---
+++ android/app/src/main/java/se/iloppis/app/utils/events/state/EventListSortType.kt
@@ -0,0 +1,7 @@
+package se.iloppis.app.utils.events.state
+
+/**
+ * List sort type
+ */
+enum class EventListSortType {
+}
---
+++ android/app/src/main/java/se/iloppis/app/utils/events/state/EventListState.kt
@@ -0,0 +1,7 @@
+package se.iloppis.app.utils.events.state
+
+/**
+ * Event list state
+ */
+class EventListState() {
+}

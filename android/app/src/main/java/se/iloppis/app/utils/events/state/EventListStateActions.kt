ff650f4311948080e50d9629fd3926b901d219a4
Xtarii <carlalvinelias@gmail.com>
2026-02-08 18:06:35 +0100

    Event list actions added

--- android/app/src/main/java/se/iloppis/app/utils/events/state/EventListStateActions.kt
+++ android/app/src/main/java/se/iloppis/app/utils/events/state/EventListStateActions.kt
@@ -1,1 +1,25 @@
 package se.iloppis.app.utils.events.state
+
+/**
+ * Event list state action
+ *
+ * Controls specified [EventListState]
+ */
+sealed class EventListStateAction {
+    /**
+     * Sets sorting method for the [EventListState]
+     */
+    data class SetSortingMethod(
+        /**
+         * Sort method to use
+         *
+         * Default is [EventListSortType.SAVED]
+         *
+         * @see EventListState.sort
+         * @see EventListState.events
+         * @see EventListSortType.SAVED
+         * @see EventListSortType
+         */
+        val sort: EventListSortType = EventListSortType.SAVED
+    ) : EventListStateAction()
+}

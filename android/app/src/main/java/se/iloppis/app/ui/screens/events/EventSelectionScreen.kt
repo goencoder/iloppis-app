52ed7581041aabfb26ce0fb09da1c1ca0ecc782e
Xtarii <carlalvinelias@gmail.com>
2026-02-08 18:13:44 +0100

    Event list state on selection screen

--- android/app/src/main/java/se/iloppis/app/ui/screens/events/EventSelectionScreen.kt
+++ android/app/src/main/java/se/iloppis/app/ui/screens/events/EventSelectionScreen.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import se.iloppis.app.domain.model.Event
+import se.iloppis.app.utils.events.state.rememberEventListState
 
 @Composable
 fun EventSelectionScreen(onAction: (event: Event) -> Unit) {
+    val state = rememberEventListState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -19,6 +23,8 @@ fun EventSelectionScreen(onAction: (event: Event) -> Unit) {
     ) {
 
         /* One page with saved events */
+
+        Text("Sort: ${state.sort}")
 
         /* One page with all events ( load this is saved is 0 ) */
 

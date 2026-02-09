972b56ff59816fab90f30ef0cbbd7832568cd84d
Xtarii <carlalvinelias@gmail.com>
2026-02-09 18:57:30 +0100

    text and button size

--- android/app/src/main/java/se/iloppis/app/ui/components/buttons/Buttons.kt
+++ android/app/src/main/java/se/iloppis/app/ui/components/buttons/Buttons.kt
@@ -1,6 +1,7 @@
 package se.iloppis.app.ui.components.buttons
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -11,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * Button component with an icon and text object
@@ -30,11 +32,13 @@ fun IconButton(
     ) {
         Icon(
             imageVector = icon,
+            modifier = Modifier.size(20.dp),
             contentDescription = stringResource(text)
         )
         Text(
             text = stringResource(text),
-            modifier = Modifier.padding(start = 5.dp)
+            modifier = Modifier.padding(start = 5.dp),
+            fontSize = 12.sp
         )
     }
 }

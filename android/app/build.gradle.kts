7cb0b96306c83f16ee88b185006e792c41c30f08
Göran Engdahl <goran@goencoder.se>
2026-09-01 23:53:36 +0200

    Configure native symbol extraction for releases

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -79,6 +79,9 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
